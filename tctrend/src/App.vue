1ae3605cf34f05f6794111798b41b5fa632476c7
aiddin <arcticdessert@gmail.com>
2022-12-12 10:03:25 +0800

    hmm

---
+++ tctrend/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+<!-- 9.34
+8.38
+8.15
+8.84
+9.36
+8.52
+9.40
+8.58
+8.44
+9.51 -->
---
+++ tctrend/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <span v-for="trend in trends" :key="trend.id">
+      <span v-if="trend.direction === 'up'" class="arrow-up"></span>
+      <span v-if="trend.direction === 'down'" class="arrow-down"></span>
+      <span v-if="trend.direction === 'unchanged' " class="unchanged"></span>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      trends: [
+        { id: 1, direction: 'up' },
+        { id: 2, direction: 'down' },
+        { id: 3, direction: 'up' },
+        { id: 4, direction: 'down' },
+        { id: 4, direction: 'unchanged' },
+
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.arrow-up:before {
+  content: '\2191';
+}
+
+.arrow-down:before {
+  content: '\2193';
+}
+
+.unchanged:before {
+  content: '-';
+}
+</style>
